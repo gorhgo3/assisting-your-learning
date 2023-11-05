96b83f900406fee18abc51f4bd49330257cbd079
Murray Sinclair <msin981@gmail.com>
2023-11-05 16:37:34 +1300

    /learning route added

--- Backend/routes/learning.js
+++ Backend/routes/learning.js
@@ -1,9 +1,11 @@
 import express from "express"
+import { checkVideo } from "../functions/index.js"
 
 const router = express.Router()
 
-router.use('/', (req, res) => {
-  res.send('this is a learning route')
+router.use('/', async (req, res) => {
+  const data = await checkVideo()
+  res.send(data)
 })
 
 
