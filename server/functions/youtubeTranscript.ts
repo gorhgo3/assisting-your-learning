21307ede1b9b4bb060d5e45852cca071d57a30e0
Murray Sinclair <msin981@gmail.com>
2023-11-12 20:11:54 +1300

    update checkVideo function

--- server/functions/youtubeTranscript.ts
+++ server/functions/youtubeTranscript.ts
@@ -1,25 +1,18 @@
 import { YoutubeTranscript } from 'youtube-transcript'
-import OpenAI from 'openai'
-import { config } from 'dotenv'
-
-config()
 
 
-export async function checkVideo (url:string) {
-  const ENDPOINT = url
+/**
+ * Fetches the transcript of a YouTube video based on the provided URL.
+ * @param {string} url - The URL of the YouTube video.
+ * @returns {Promise<string>} - A promise that resolves to the transcript of the video.
+ */
+export async function checkVideo(url:string) {
+  const ENDPOINT = url;
   try {
-    // fetch transcript
-    const data = YoutubeTranscript.fetchTranscript(ENDPOINT).then((data) => {
-      const dataTranscript:[] = []
-      data.forEach((transcriptLine) => {
-        dataTranscript.push(transcriptLine.text)
-      })
-      const dataTranscript2 = dataTranscript.join(' ')
-      return dataTranscript2
-    })
-    return data
+    const data = await YoutubeTranscript.fetchTranscript(ENDPOINT);
+    return data;
   } catch (err) {
-    console.log('Transcript may be disabled for this video')
-    console.log(err)
+    console.log('Transcript may be disabled for this video');
+    console.log(err);
   }
 }
