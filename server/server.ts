cab67690baf04a6dfc8608aad5616c47b47ce993
Murray Sinclair <msin981@gmail.com>
2023-11-12 15:51:20 +1300

    include express.json() middleware for parsing requests

--- server/server.ts
+++ server/server.ts
@@ -1,21 +1,18 @@
 import express from 'express';
 import cors from 'cors';
-import router from './routes/learning.js';
-import initialTest from './functions/fireStore.js';
+import router from './routes/learning.ts';
+// import initialTest from './functions/fireStore.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Set up CORS middleware before defining routes
 app.use(cors());
+app.use(express.json());
 
 // await initialTest()
 
 app.use('/learning', router);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
