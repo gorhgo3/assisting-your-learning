e18bd61595e1925275380790e81e5228e6baf624
Murray Sinclair <msin981@gmail.com>
2023-11-12 20:33:11 +1300

    update post'/' to generate transcript and ai response

--- server/routes/learning.ts
+++ server/routes/learning.ts
@@ -1,35 +1,22 @@
-import express from "express"
-import { checkVideo } from "../functions/youtubeTranscript.js"
-import runAI from "../functions/aiYoutube.js";
+import express from 'express'
+import { checkVideo } from '../functions/youtubeTranscript.js'
+import runAI from '../functions/aiYoutube.js'
+import { TranscriptResponse } from 'youtube-transcript'
 
 const router = express.Router()
 
-// router.use(`/`, async (req, res) => {
-//   const url = req.query.url
-//   console.log(url);
-//   const data = await checkVideo(url)
-//   res.send(data)
-// })
+
 router.post(`/`, async (req, res) => {
   try {
     const jsonData = req.body.url
-    // console.log(jsonData)
-    // Process the JSON data as needed
-    const data = await checkVideo(jsonData)
-    // console.log(data);
-    
-    const summary = await runAI(data)
-
-    console.log(summary);
-    
-
-    // Send a response back if required
-    // res.status(200).json(summary);
+    const data: TranscriptResponse[] | undefined = await checkVideo(jsonData)
+    const summary = data?.map((line) => line.text).join(' ')
+    const response = await runAI(summary)
+    res.status(200).json(response)
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error(error)
+    res.status(500).json({ error: 'An error occurred' })
   }
-});
-
+})
 
 export default router
