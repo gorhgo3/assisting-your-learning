f26d364f1b1142e533cb4a5eafa9355c6644afee
Murray Sinclair <msin981@gmail.com>
2023-11-12 14:37:37 +1300

    adding footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2023 Your App Name. All rights reserved.</p>
+      <p>Privacy Policy | Terms of Service | Contact Us</p>
+    </div>
+  )
+}
+
+export default Footer
