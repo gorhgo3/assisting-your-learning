06ebbfb024170c10ad39fd2089eaf59393f87cee
Murray Sinclair <msin981@gmail.com>
2023-11-12 15:50:50 +1300

    update index.js to .ts

---
+++ client/src/api/index.ts
@@ -0,0 +1,10 @@
+import superagent from "superagent";
+
+const serverUrl = 'http://localhost:3000'
+
+export async function getYoutubeTranscript (url: string) {
+  const test = await superagent.post(serverUrl + '/learning').send({url})
+  console.log(test);
+  
+  return test
+} 
