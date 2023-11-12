b1a508f0c611c455100e0756e3314b21495cab02
Murray Sinclair <msin981@gmail.com>
2023-11-12 21:35:49 +1300

    loading and analyseData results rendering inc state loading

--- client/src/components/YoutubeOverview.tsx
+++ client/src/components/YoutubeOverview.tsx
@@ -3,18 +3,32 @@ import { useState } from 'react'
 import { getYoutubeTranscript } from '../api'
 
 function YoutubeOverview() {
-  const [analyseData, setAnalysiseData] = useState('')
+  const [analyseData, setAnalyseData] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
 
+  // generate youtube transcript of the youtube video and set analyseData to result
   async function getAnalysis(url: string) {
+    setLoading(true)
     const data = await getYoutubeTranscript(url)
-    setAnalysiseData(data)
+    setAnalyseData(data)
+    setLoading(false)
   }
 
   return (
     <div className="youtube-overview">
       <h4>Enter Youtube URL you want to watch</h4>
       <YoutubeForm handleSubmit={getAnalysis} />
-      <p>{analyseData}</p>
+      {loading && <h4>Loading...</h4>}
+      {analyseData && (
+        // handle the youtube responses here:
+        <div className="">
+          <p>{analyseData}</p>
+          <h4>Want to work with this response?</h4>
+          <button>Generate a 3 hour study session</button>
+          <button>Store this as a completed topic</button>
+          <button>Question this response</button>
+        </div>
+      )}
     </div>
   )
 }
