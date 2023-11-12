5c83dc8b8fda5779151b63fa91406a8482cd7672
Murray Sinclair <msin981@gmail.com>
2023-11-12 13:11:35 +1300

    add styles directory in clientside

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
 // import HomePage from './pages/HomePage.tsx'
 // import './App.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './clientRouting.tsx'
+import router from './routing.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
