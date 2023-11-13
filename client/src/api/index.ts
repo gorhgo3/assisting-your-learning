705586735ab40312d5196060a340775f8f2e8304
Murray Sinclair <msin981@gmail.com>
2023-11-13 22:30:17 +1300

    set up planning and questioning of clientside api

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -9,14 +9,20 @@ export async function getYoutubeTranscript(url: string) {
   return test.body
 }
 
-export async function getStudySessionPlan() {
+export async function getStudySessionPlan(analysis: string) {
   console.log('activating the study session')
+  return superagent
+    .get(serverUrl + '/learning/v1/openAI/study_session')
+    .send(analysis)
 }
 
 export async function addToStudyHistory() {
   console.log('adding to study history')
 }
 
-export async function questionResponse() {
+export async function questionResponse(analysis: string, question: string) {
   console.log('questioning the response')
+  return superagent
+    .get(serverUrl + '/learning/v1/openAI/question')
+    .send({ data: [analysis, question] })
 }
