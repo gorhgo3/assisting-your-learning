cfc4360de61d2b195a43b007c8749831b070e825
Murray Sinclair <msin981@gmail.com>
2023-11-13 21:59:34 +1300

    refactor new function name

--- server/routes/learning.ts
+++ server/routes/learning.ts
@@ -1,17 +1,16 @@
 import express from 'express'
 import { checkVideo } from '../functions/youtubeTranscript.js'
-import runAI from '../functions/aiYoutube.js'
+import reviewTranscript from '../functions/openAi.js'
 import { TranscriptResponse } from 'youtube-transcript'
 
 const router = express.Router()
-
 
 router.post(`/`, async (req, res) => {
   try {
     const jsonData = req.body.url
     const data: TranscriptResponse[] | undefined = await checkVideo(jsonData)
     const summary = data?.map((line) => line.text).join(' ')
-    const response = await runAI(summary)
+    const response = await reviewTranscript(summary)
     res.status(200).json(response)
   } catch (error) {
     console.error(error)
