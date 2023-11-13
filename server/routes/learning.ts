130a7a58c4dfd7ddf5e270c701ede8358d54ec55
Murray Sinclair <msin981@gmail.com>
2023-11-13 22:17:05 +1300

    new server endpoints for study session and queston

--- server/routes/learning.ts
+++ server/routes/learning.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import { checkVideo } from '../functions/youtubeTranscript.js'
-import reviewTranscript from '../functions/openAi.js'
+import reviewTranscript, { newStudySession, questionResponse } from '../functions/openAi.js'
 import { TranscriptResponse } from 'youtube-transcript'
 
 const router = express.Router()
@@ -17,5 +17,30 @@ router.post(`/transcript`, async (req, res) => {
     res.status(500).json({ error: 'An error occurred' })
   }
 })
+
+router.get('/study_session', async(req, res) => {
+  try {
+    const analysis = req.body.analysis
+    const res = await newStudySession(analysis)
+    res.status(200).json(res)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({error: 'An error occured'})
+  }
+})
+
+router.get('/question', async(req, res) => {
+  try {
+    const question = req.body.question
+    const res = await questionResponse(question)
+    res.status(200).json(res)
+  } catch(err) {
+    console.error(err)
+    res.status(500).json({error: 'An error occured'})
+  }
+})
+
+
+
 
 export default router
