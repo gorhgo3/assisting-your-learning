807c81eefc425498ed94aa635fae6f93ccb3ec90
Murray Sinclair <msin981@gmail.com>
2023-11-13 22:03:17 +1300

    new function quesionAIReponse

--- server/functions/openAi.ts
+++ server/functions/openAi.ts
@@ -4,7 +4,7 @@ import OpenAI from 'openai'
 config()
 
 export async function reviewTranscript(data: any) {
-  const analyseTranscript = `
+  const message = `
   can you summarise the content of this transcript for a youtube video and bulletpoint the key points, and importantly explain if this is up to date to modern learning to code practises:
   ${data}`
   // return message
@@ -13,7 +13,7 @@ export async function reviewTranscript(data: any) {
       apiKey: process.env.OPENAI_API_KEY,
     })
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: analyseTranscript }],
+      messages: [{ role: 'user', content: message }],
       model: 'gpt-3.5-turbo',
     })
     return chatCompletion.choices[0].message.content
@@ -25,7 +25,7 @@ export async function reviewTranscript(data: any) {
 
 // Generate a 3 hour study session
 export async function newStudySession(analysis:string) {
-  const threeHoursStudy = `
+  const message = `
   can you generate a series of topics that can take up to 3 hours total to get a better understanding of the content of this video- generate for me a study session of up to 3 hours from this overview:
   ${analysis}
   `
@@ -34,7 +34,7 @@ export async function newStudySession(analysis:string) {
       apiKey: process.env.OPENAI_API_KEY,
     })
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: analysis }],
+      messages: [{ role: 'user', content: message }],
       model: 'gpt-3.5-turbo',
     })
     return chatCompletion.choices[0].message.content
@@ -43,7 +43,26 @@ export async function newStudySession(analysis:string) {
   }
 }
 
-// Store this as a completed topic
+
 // Question this response
+export async function questionResponse(data: any) {
+  const message = `
+  can you please answer my question to this analysis:
+  ${data.analysis}
+  ${data.question}
+  `
+  try {
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    })
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: message }],
+      model: 'gpt-3.5-turbo',
+    })
+    return chatCompletion.choices[0].message.content
+  } catch (err) {
+    return err
+  }
+}
 
 export default reviewTranscript
