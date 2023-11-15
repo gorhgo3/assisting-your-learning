beaa5e5012345ac21668457c4ab84da61ce320f2
Murray Sinclair <msin981@gmail.com>
2023-11-16 11:26:47 +1300

    adding typescript to .post study_session

--- server/routes/learning.ts
+++ server/routes/learning.ts
@@ -1,7 +1,12 @@
 import express from 'express'
 import { checkVideo } from '../functions/youtubeTranscript.js'
-import reviewTranscript, { newStudySession, questionResponse } from '../functions/openAi.js'
+import reviewTranscript, {
+  newStudySession,
+  questionResponse,
+} from '../functions/openAi.js'
 import { TranscriptResponse } from 'youtube-transcript'
+import { OpenAI } from 'openai'
+import { aiMessage } from '../models/ai.js'
 
 const router = express.Router()
 
@@ -11,6 +16,7 @@ router.post(`/transcript`, async (req, res) => {
     const data: TranscriptResponse[] | undefined = await checkVideo(jsonData)
     const summary = data?.map((line) => line.text).join(' ')
     const response = await reviewTranscript(summary)
+    console.log(response)
     res.status(200).json(response)
   } catch (error) {
     console.error(error)
@@ -18,29 +24,29 @@ router.post(`/transcript`, async (req, res) => {
   }
 })
 
-router.get('/study_session', async(req, res) => {
-  try {
-    const analysis = req.body.analysis
-    const res = await newStudySession(analysis)
-    res.status(200).json(res)
-  } catch(err) {
-    console.error(err)
-    res.status(500).json({error: 'An error occured'})
-  }
+router.post('/study_session', async (req, res) => {
+  await newStudySession(req.body.analysis)
+    .then((data: OpenAI.Chat.Completions.ChatCompletion) => {
+      const studySession: aiMessage = {
+        action: 'study_session',
+        answer: data.choices[0].message.content as string,
+      }
+      res.status(200).json(studySession)
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'An error occured' + err })
+    })
 })
 
-router.get('/question', async(req, res) => {
+router.get('/question', async (req, res) => {
   try {
     const question = req.body.question
     const res = await questionResponse(question)
     res.status(200).json(res)
-  } catch(err) {
+  } catch (err) {
     console.error(err)
-    res.status(500).json({error: 'An error occured'})
+    res.status(500).json({ error: 'An error occured' })
   }
 })
-
-
-
 
 export default router
