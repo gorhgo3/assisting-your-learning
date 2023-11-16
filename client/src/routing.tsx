7f72f4a5ea89253edb6210912c4a2e830a52a3d1
Murray Sinclair <msin981@gmail.com>
2023-11-16 23:43:11 +1300

    simplified routing

--- client/src/routing.tsx
+++ client/src/routing.tsx
@@ -6,15 +6,15 @@ import {
 import App from './components/App'
 import HomePage from './pages/Home'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/">
-        <Route path="" element={<HomePage />} />
-      </Route>
-      <Route path="/login/">
-        <Route path="app" element={<App />} />
+        <Route path="test" element={<HomePage />} />
+        <Route path="personal/app" element={<App />} />
+        {/* <Route path="login/" element={<Header />} /> */}
       </Route>
     </>
   )
