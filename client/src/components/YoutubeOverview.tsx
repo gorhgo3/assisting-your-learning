08d1d6ff6238353d3c9bbfcfbaa2d548a0b70d9d
Murray Sinclair <msin981@gmail.com>
2023-11-16 13:09:24 +1300

    remove useQuery

--- client/src/components/YoutubeOverview.tsx
+++ client/src/components/YoutubeOverview.tsx
@@ -2,7 +2,7 @@ import YoutubeForm from './YoutubeForm'
 import { useState } from 'react'
 import {
   getYoutubeTranscript,
-  getStudySessionPlan,
+  getStudySession,
   addToStudyHistory,
   questionResponse,
 } from '../api'
@@ -19,54 +19,65 @@ import { useQuery } from '@tanstack/react-query'
 // Please provide the transcript, and I'll be happy to assist you further.
 // `
 
-const DEFAULT_URL = "https://www.youtube.com/watch?v=QIyc6NKS5J0&ab_channel=ThePrimeagen"
+const DEFAULT_URL =
+  'https://www.youtube.com/watch?v=QIyc6NKS5J0&ab_channel=ThePrimeagen'
 
-const DEFAULT_RESULT = "The speaker has been programming professionally for almost two decades and has experience in various programming languages. - In 2007-2008, the speaker was primarily using Windows for programming but wanted to get better at Linux. - They initially avoided using Linux because they perceived it as difficult and outdated. - The speaker realized that their mentality of assuming that things with friction or difficulty were wrong hindered their progress. - They regret not being open to learning and using different tools and technologies earlier in their career. - The speaker shares a story about an interview where they wrote a complex Java code to solve a problem that could have been easily solved with grep. - They emphasize the importance of being open to uncomfortable and unfamiliar tools and technologies. - The speaker encourages viewers to try different editors, operating systems, and frameworks to expand their knowledge and problem-solving abilities. - They highlight the importance of gaining diverse experiences and perspectives to become a better engineer. - The video concludes with a call to subscribe to the channel and share personal stories. As for the relevance to modern learning to code practices, the video emphasizes the importance of being open to new technologies and tools, which is still relevant today. The speaker encourages viewers to expand their knowledge and problem-solving abilities by trying different editors and frameworks, which aligns with the idea of learning multiple tools in order to become a well-rounded developer. However, it's worth noting that the specific tools and technologies mentioned (e.g., NetBeans, vim, grep) may not be the most up-to-date or commonly used in the industry today"
-
+const DEFAULT_RESULT =
+  "The speaker has been programming professionally for almost two decades and has experience in various programming languages. - In 2007-2008, the speaker was primarily using Windows for programming but wanted to get better at Linux. - They initially avoided using Linux because they perceived it as difficult and outdated. - The speaker realized that their mentality of assuming that things with friction or difficulty were wrong hindered their progress. - They regret not being open to learning and using different tools and technologies earlier in their career. - The speaker shares a story about an interview where they wrote a complex Java code to solve a problem that could have been easily solved with grep. - They emphasize the importance of being open to uncomfortable and unfamiliar tools and technologies. - The speaker encourages viewers to try different editors, operating systems, and frameworks to expand their knowledge and problem-solving abilities. - They highlight the importance of gaining diverse experiences and perspectives to become a better engineer. - The video concludes with a call to subscribe to the channel and share personal stories. As for the relevance to modern learning to code practices, the video emphasizes the importance of being open to new technologies and tools, which is still relevant today. The speaker encourages viewers to expand their knowledge and problem-solving abilities by trying different editors and frameworks, which aligns with the idea of learning multiple tools in order to become a well-rounded developer. However, it's worth noting that the specific tools and technologies mentioned (e.g., NetBeans, vim, grep) may not be the most up-to-date or commonly used in the industry today"
 
 function YoutubeOverview() {
   const [url, setUrl] = useState<string>(DEFAULT_URL)
-  const [studySession, setStudySession] = useState(DEFAULT_RESULT)
-  const {
-    data,
-    isLoading,
-    isError,
-    refetch,
-  } = useQuery({
-    queryKey: ['youtubeTranscript'],
-    queryFn: () => getYoutubeTranscript(url),
-    enabled: false,
-  })
+  const [transcript, setTranscript] = useState<string>('')
+  const [studySession, setStudySession] = useState<string[]>([])
+
+  // const { data, isLoading, isError, refetch } = useQuery({
+  //   queryKey: ['youtubeTranscript'],
+  //   queryFn: () => getYoutubeTranscript(url),
+  //   enabled: false,
+  // })
 
   async function getAnalysis(url: string) {
     setUrl(url)
-    refetch()
+    getYoutubeTranscript(url).then((data) => {
+      setTranscript(data)
+    })
+    // refetch()
   }
 
   async function generateStudySession(session: any) {
-    console.log(session)
-    const response = await getStudySessionPlan(studySession)
-    setStudySession(response)
+    console.log('data')
+    try {
+      await getStudySession(transcript).then((data) => {
+        const points = data.answer.split('-')
+        setStudySession(points)
+        return points
+      })
+    } catch (error) {
+      console.error(error)
+    }
   }
+  console.log(studySession)
 
   return (
     <div className="youtube-overview">
       <h4>Enter Youtube URL you want to watch</h4>
       <YoutubeForm handleSubmit={getAnalysis} />
-      {isLoading && <h4>Loading...</h4>}
-      {isError && <h4>Error</h4>}
+      {/* {isLoading && <h4>Loading...</h4>} */}
+      {/* {isError && <h4>Error</h4>} */}
       <button onClick={generateStudySession}>
         Generate a 3 hour study session
       </button>
-      {data && (
+      {transcript && (
         // handle the youtube responses here:
         <div className="">
-          <p>{data}</p>
+          {transcript}
           <h4>Want to work with this response?</h4>
           <button onClick={addToStudyHistory}>
             Store this as a completed topic
           </button>
-          <button onClick={questionResponse}>Question this response</button>
+          <button onClick={() => questionResponse}>
+            Question this response
+          </button>
         </div>
       )}
       {studySession}
