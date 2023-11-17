17f6a13da7e60f14d5b1b384fdae7e3ecad0bd6b
Murray Sinclair <msin981@gmail.com>
2023-11-18 10:36:55 +1300

    update header links

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="header">
       <h3 style={{display:"inline"}}>StudyBuddy</h3>
       <Link to={'/personal/app'}>Personal</Link>
-      <Link to={'/test'}>Explore</Link>
+      <Link to={'/'}>Explore</Link>
       <Link to={'/login'}>Login(notWorking)</Link>
     </div>
   )
