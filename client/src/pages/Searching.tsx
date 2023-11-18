b7d839ec649c8c24763274c93ff51c44c612a009
Murray Sinclair <msin981@gmail.com>
2023-11-18 15:25:38 +1300

    new page firstpass searching.tsx

---
+++ client/src/pages/Searching.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import YoutubeOverview from '../components/YoutubeOverview'
+import Header from '../components/Header'
+
+
+function Searching() {
+  return (
+    <>
+    <Header/>
+    <YoutubeOverview/>
+    </>
+  )
+}
+
+export default Searching
