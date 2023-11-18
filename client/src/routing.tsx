d3363832d7aadf7d972f65c2757c2429367e3904
Murray Sinclair <msin981@gmail.com>
2023-11-18 15:25:16 +1300

    new route for searching

--- client/src/routing.tsx
+++ client/src/routing.tsx
@@ -4,14 +4,14 @@ import {
   Route,
 } from 'react-router-dom'
 import App from './components/App'
-import HomePage from './pages/Home'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import HomePage from './pages/Home's
+import Searching from './pages/Searching'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<HomePage />} />
+      <Route path="/searching" element={<Searching />} />
       <Route path="personal/app" element={<App />} />
     </>
   )
