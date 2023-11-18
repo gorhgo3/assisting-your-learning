20daa124f2327bb3682ecff72aaf9728f5c212dc
Murray Sinclair <msin981@gmail.com>
2023-11-18 22:58:41 +1300

    SingleProposals handleClick to increase studySession

--- client/src/components/SingleProposal.tsx
+++ client/src/components/SingleProposal.tsx
@@ -1,17 +1,36 @@
 import { useState } from 'react'
+import { addStudyElement } from '../api'
+import { useQueryClient } from '@tanstack/react-query'
 
 interface Props {
   proposal: string
 }
 
+interface UserDetails {
+  nickname: string
+  ID: string
+  studying: string
+}
+
 function SingleProposal(props: Props) {
-  const [active, setActove] = useState(false)
+  const queryClient = useQueryClient()
+  const [active, setActive] = useState(false)
   const { proposal } = props
 
+  // add to the firestore DB
+  function handleClick() {
+    setActive(!active)
+    const data = queryClient.getQueryData(['user_id']) as UserDetails
+    addStudyElement({ data: proposal, id: data?.ID })
+  }
+
   return (
-    <div key={proposal} className={`${active ? 'study-topic_active' : ''} study-topic`}>
+    <div
+      key={proposal}
+      className={`${active ? 'study-topic_active' : ''} study-topic`}
+    >
       <p>{proposal}</p>
-      <button onClick={() => setActove(!active)}>add</button>
+      <button onClick={handleClick}>add</button>
     </div>
   )
 }
