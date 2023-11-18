f61d7f0345305eb723c5aa766c2e51a085a731d7
Murray Sinclair <msin981@gmail.com>
2023-11-18 23:00:12 +1300

    temp function getUserDetails added

---
+++ client/src/functions/userDetails.ts
@@ -0,0 +1,8 @@
+export async function getUserDetails () {
+  const TEMP_USER_PROFILE = {
+    nickname: 'Gorhgo3',
+    ID: '1111aaaabbbbb',
+    studying: 'full stack web development',
+  }
+  return TEMP_USER_PROFILE
+}
