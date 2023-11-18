40285cb64c7c759a97475f17084aa6a4340c4410
Murray Sinclair <msin981@gmail.com>
2023-11-19 00:32:10 +1300

    include profile route

--- client/src/routing.tsx
+++ client/src/routing.tsx
@@ -3,16 +3,16 @@ import {
   createRoutesFromElements,
   Route,
 } from 'react-router-dom'
-import App from './components/App'
 import HomePage from './pages/Home'
 import Searching from './pages/Searching'
+import Profile from './pages/Profile'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<HomePage />} />
       <Route path="/searching" element={<Searching />} />
-      <Route path="personal/app" element={<App />} />
+      <Route path="personal/app" element={<Profile />} />
     </>
   )
 )
