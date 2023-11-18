525b7574e94876fa513d406f3a27d470b2c0604a
Murray Sinclair <msin981@gmail.com>
2023-11-19 00:36:00 +1300

    profile maps through user study topics

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,27 +1,33 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
-import { getUserDetails } from '../functions/userDetails'
+import Header from '../components/Header'
+import { fetchUserDetails } from '../api'
 
 function Profile() {
   const { data, isLoading, isError } = useQuery({
-    queryFn: getUserDetails,
+    queryFn: () => fetchUserDetails(),
     queryKey: ['user'],
   })
 
   if (isLoading) return <h1>Loading..</h1>
   if (isError) return <h1>Error</h1>
-  if (data)
+  if (data) {
+    const { nickname, studying, study_topics } = data
     return (
       <>
+        <Header />
         <div className="content">
-          <h1>{data.nickname}</h1>
+          <h1>{nickname}</h1>
           <p>
             We noticed you last set your study status to:
-            <strong>{data.studying}</strong>
+            <strong>{studying}</strong>
+            {study_topics.map((topic: string) => (
+              <p key={topic}>{topic}</p>
+            ))}
           </p>
         </div>
       </>
     )
+  }
 }
 
-export default HomePage
+export default Profile
