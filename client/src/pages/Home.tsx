9712d161e742b2018a6851d5837e2a1ad7f83bdc
Murray Sinclair <msin981@gmail.com>
2023-11-19 21:36:52 +1300

    TSX changes to Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,21 +7,22 @@ function HomePage() {
     <>
       <Header />
       <div className="content">
-        <h1>Welcome to StudyBuddy</h1>
-        <p>
+        <div className="">
+        <h1 className="title display-1 mt-5 mx-5">Welcome to Murrays Study-Buddy</h1>
+        <h4 className="title-sub m-5">
+          <del className='title-sub--negative'>Watch the whole video</del> nope.✅ Develop a Plan✅ Spend time productively✅
+        </h4>
+        <p className="m-5">
           Let AI assist in the process of learning. Get the most from your
-          invested study hours.
-        </p>
-        <p>
+          invested study hours. <br />
           Save time by filtering YouTube videos relevant to today's
-          ever-changing tech industry.
-        </p>
-        <p>
+          ever-changing tech industry. <br />
           Get started with our new tool. Only watch YouTube tutorials that keep
           up with the latest tech standards.
         </p>
-        <Link to="/login/app">Login</Link>
-        <YoutubeOverview />
+        {/* <Link to="/login/app">Login</Link>
+        <YoutubeOverview /> */}
+        </div>
       </div>
     </>
   )
