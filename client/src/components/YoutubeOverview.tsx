290e8d5dc158cb15aae43714fe02fcb9e140541e
Murray Sinclair <msin981@gmail.com>
2023-11-19 21:35:22 +1300

    update summary to reflect the new model

--- client/src/components/YoutubeOverview.tsx
+++ client/src/components/YoutubeOverview.tsx
@@ -9,16 +9,18 @@ import {
 import StudyProposals from './StudyProposals'
 import { getUserDetails } from '../functions/userDetails'
 import { useQuery } from '@tanstack/react-query'
+import { VideoSummary } from '../../../server/models/aiModels'
+import TEST_SUMMARY from '../styles/testData'
 
 function YoutubeOverview() {
   const [url, setUrl] = useState<string>('')
-  const [transcript, setTranscript] = useState<string>('')
+  const [transcript, setTranscript] = useState<VideoSummary>(TEST_SUMMARY)
   const [studySession, setStudySession] = useState<string[] | null>()
 
   const { data } = useQuery({
     queryFn: getUserDetails,
     queryKey: ['user_id'],
-  })  
+  })
 
   async function getAnalysis(url: string) {
     setUrl(url)
@@ -27,9 +29,9 @@ function YoutubeOverview() {
     })
   }
 
-  async function generateStudySession(session: any) {
+  async function generateStudySession() {
     try {
-      await getStudySession(transcript).then((data) => {
+      await getStudySession(url).then((data) => {
         const points = data.answer.split('\n')
         setStudySession(points)
         return points
@@ -43,14 +45,16 @@ function YoutubeOverview() {
     <div className="youtube-overview">
       <YoutubeForm handleSubmit={getAnalysis} />
       {transcript && (
-        // YOUTUBE SUMMARY AFTER URL SUBMIT
-        <>
-          <div className="ai-analysis-overview">
-            <h3>Here is a Summary of the Youtube Video:</h3>
-            <p>{transcript}</p>
-          </div>
+        <div className="ai-analysis-overview">
+          <h3>Here is a Summary of the Youtube Video:</h3>
+          <h4>Summary:</h4>
+          <p>{transcript.summary}</p>
+          <h4>Relevency on {new Date().toString()}:</h4>
+          <p>{transcript.relevancy}</p>
+          <h4>Technical aspects:</h4>
+          <p>{transcript.technical}</p>
           <button onClick={generateStudySession}>Generate study session</button>
-        </>
+        </div>
       )}
       {studySession && <StudyProposals study={studySession} />}
     </div>
