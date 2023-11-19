d6291aab9c7e2cba9ab477f6cdfe5f24c68ee4ac
Murray Sinclair <msin981@gmail.com>
2023-11-19 21:35:35 +1300

    default test summary for presenting?

---
+++ client/src/styles/testData.tsx
@@ -0,0 +1,14 @@
+import { VideoSummary } from '../../../server/models/aiModels'
+
+const TEST_SUMMARY: VideoSummary = {
+  summary:
+    'The speaker shares their experience of avoiding certain technologies and ways of solving problems because they believed they were superior. They reflect on how this mentality hindered their growth as an engineer and resulted in embarrassing moments, such as a job interview where they unnecessarily wrote complex code instead of using a simple command-line tool. They encourage the audience to be open-minded, try new things, and embrace discomfort in order to become better engineers.',
+
+  technical:
+    'The video discusses the importance of being open to learning new technologies and approaches in programming. The speaker shares their personal experience of being resistant to using Linux and command-line tools, believing that they were outdated and unnecessary. However, they later realized that their reluctance to step out of their comfort zone hindered their growth as an engineer. The video emphasizes the value of exploring different tools and technologies to become a better programmer and problem solver. The speaker encourages viewers to try out new editors, programming languages, and frameworks to expand their knowledge and improve their skills.',
+
+  relevancy:
+    'Based on the given content, it is difficult to determine whether the content is up to date with modern learning to code practices. The speaker talks about their own experiences and mindset from a decade ago, so it may not necessarily reflect current practices. However, the main message of the video seems to be about the importance of being open to new technologies, tools, and approaches in order to become a better engineer. This concept is still relevant in the present day.',
+}
+
+export default TEST_SUMMARY
